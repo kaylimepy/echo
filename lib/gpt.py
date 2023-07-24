3f3e0223f0aa8bee49d6016f782db0f0c0dcf2ad
Ryan Beasse <ryan@emergeknowledge.com>
2023-07-24 16:10:56 -0500

    Add support for APOD understanding the current date
    Also allow Echo to grab historical APOD images

--- lib/gpt.py
+++ lib/gpt.py
@@ -1,40 +1,53 @@
 import openai
 import requests
+import json
 import lib.config as config
+
+from datetime import datetime
 
 openai.api_key = config.read('openai', 'api_key')
 
 
-def get_apod():
+def get_apod(date: str) -> str:
     '''Get the Astronomy Picture of the Day from NASA's API'''
     url     = config.read('nasa', 'url')
     api_key = config.read('nasa', 'api_key')
 
-    response = requests.get(url, params={'api_key': api_key})
+    response = requests.get(url, params={'api_key': api_key, 'date': date})
 
     return response.json()['hdurl']
 
 
 def chat(message: str) -> str:
     '''Chat with GPT-3'''
-    function_map = {
-        'get_apod': get_apod,
-    }
+    function_map = {'get_apod': get_apod}
 
     response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=[config.read('openai', 'pre_prompt'),{"role": "user", "content": message}],
+                messages=__messages(message),
                 functions=config.read('openai', 'functions'),
                 function_call='auto'
             )
-    
     response_message = response.choices[0]['message']
 
-    if response_message.get('function_call'):
+    if 'function_call' in response_message:
         function_name = response_message['function_call']['name']
-        print(response_message)
+        function_args = json.loads(response_message['function_call']['arguments'])
 
-        return function_map[function_name]()
-    
+        return function_map[function_name](date=function_args['date'])
     else:
         return response_message['content']
+
+def __messages(message):
+    '''Create the messages list for the GPT API'''
+    messages = []
+
+    # GPT-3 only has access to the data that it was trained on, and that data has a cuttoff date.. so if we want
+    # to be able to talk about the current date, we need to add it to the messages list.
+    current_date = f'Todays date is {datetime.now().strftime("%d/%m/%Y")}. Use this date for any date related questions.'
+
+    messages.append(config.read('openai', 'pre_prompt'))
+    messages.append({'role': 'system', 'content': current_date})
+    messages.append({'role': 'user', 'content': message})
+
+    return messages
